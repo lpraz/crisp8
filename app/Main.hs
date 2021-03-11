5f156e89017dfb7faf9a25e9633040fba09ca550
lpraz <lpraz@mts.net>
2021-03-10 21:31:35 -0600

    Got IBM logo ROM working

--- app/Main.hs
+++ app/Main.hs
@@ -7,6 +7,11 @@ import Control.Monad (unless)
 import qualified Data.ByteString as B
 import Foreign.C.Types (CInt(..))
 import Data.Int (Int32)
+import Data.Word (Word8)
+import qualified Data.Array.Unboxed as UA
+import Control.Concurrent
+
+import qualified Machine as M
 
 main :: IO ()
 main = do
@@ -14,47 +19,64 @@ main = do
     window <- createWindow "crisp8" defaultWindow
     windowSize window $= chip8DisplaySize ^* 8
     renderer <- createRenderer window (-1) defaultRenderer
-    appLoop renderer
+    rom <- B.readFile "roms/ibm.ch8"
+    let machine = M.load rom M.makeMachine
+    appLoop machine renderer
     destroyWindow window
 
-appLoop :: Renderer -> IO ()
-appLoop renderer = do
-    exit <- wasQPressed renderer
-    updateDisplay renderer
-    unless exit (appLoop renderer)
+appLoop :: M.Machine -> Renderer -> IO ()
+appLoop machine renderer = do
+    threadDelay $ 1000000 `div` 700
+    either handleError keepLooping (M.cycle machine)
+    where
+      handleError msg = do
+        print msg
+        updateDisplay machine renderer
+        exit <- userAskedToExit renderer
+        unless exit (appLoop machine renderer)
+      keepLooping newMachine = do
+        updateDisplay machine renderer
+        exit <- userAskedToExit renderer
+        unless exit (appLoop newMachine renderer)
 
-updateDisplay :: Renderer -> IO ()
-updateDisplay renderer = do
+updateDisplay :: M.Machine -> Renderer -> IO ()
+updateDisplay machine renderer = do
     rendererDrawColor renderer $= offColor
     clear renderer
-    texture <- dummyTexture renderer
+    texture <- getTexture (M.screen machine) renderer
     copy renderer texture Nothing Nothing
     destroyTexture texture
     present renderer
 
-wasQPressed :: Renderer -> IO Bool
-wasQPressed renderer = do
+getTexture :: UA.UArray (Word8, Word8) Bool -> Renderer -> IO Texture
+getTexture screen renderer = do
+    texture <- createTexture renderer RGB24 TextureAccessStreaming chip8DisplaySize
+    let screenBytes = getScreenByteString screen
+    updateTexture texture Nothing screenBytes (fromIntegral (M.displayWidth * 3))
+
+getScreenByteString :: UA.UArray (Word8, Word8) Bool -> B.ByteString
+getScreenByteString screen = B.pack bytes
+    where
+      -- We have (x, y), but Haskell's (a, b) Ord gives pixels in column order
+      -- instead of row order when we use UA.elems.
+      -- Flip coords to order correctly
+      flipped = UA.ixmap 
+        ((0, 0), (M.displayHeight - 1, M.displayWidth - 1))
+        (\(x, y) -> (y, x))
+        screen
+      bools = UA.elems flipped
+      bytes = concatMap (\b -> if b then [255, 255, 255] else [0, 0, 0]) bools
+
+userAskedToExit :: Renderer -> IO Bool
+userAskedToExit renderer = do
     events <- pollEvents
     let eventIsQPress event =
           case eventPayload event of
-            KeyboardEvent keyboardEvent ->
-              keyboardEventKeyMotion keyboardEvent == Pressed &&
-              keysymKeycode (keyboardEventKeysym keyboardEvent) == KeycodeQ
+            WindowClosedEvent windowClosedEvent -> True
             _ -> False
     return $ any eventIsQPress events
   
 offColor = V4 0 0 0 0
 onColor = V4 255 255 255 0
 
-chip8DisplayWidth = 64 :: Int
-chip8DisplayHeight = 32 :: Int
-chip8TotalPixels = chip8DisplayWidth * chip8DisplayHeight
-chip8DisplaySize = V2 (fromIntegral chip8DisplayWidth) (fromIntegral chip8DisplayHeight)
-
-dummyTexture :: Renderer -> IO Texture
-dummyTexture renderer = do
-    texture <- createTexture renderer RGBA8888 TextureAccessStreaming chip8DisplaySize
-    updateTexture texture Nothing dummyDisplay (fromIntegral (chip8DisplayWidth * 4))
-
-dummyDisplay :: B.ByteString
-dummyDisplay = B.pack $ replicate (chip8TotalPixels `div` 2 * 4) 255
+chip8DisplaySize = V2 (fromIntegral M.displayWidth) (fromIntegral M.displayHeight)
--- src/Machine.hs
+++ src/Machine.hs
@@ -2,30 +2,39 @@ module Machine where
 
 import Data.Array.Unboxed as UA
 import Data.Word (Word8, Word16)
-import qualified Timer as T
-import qualified ProgramCounter as PC
+import Data.Bits (shift, (.&.))
+import qualified Data.ByteString as B
+import Numeric (showHex)
 
--- TODO: compat with SuperCHIP-8, XO-CHIP (use GADT?)
+-- TODO: remove this
+import qualified Debug.Trace as DB
+
+-- TODO: compat with SUPER-CHIP, XO-CHIP (use GADT?)
 data Machine = Machine
     { ram :: UA.UArray Word16 Word8 -- up to 4kb for original CHIP-8
     , screen :: UArray (Word8, Word8) Bool -- 64x32x1bpp for original CHIP-8
-    , pc :: PC.ProgramCounter
-    , i :: Word16
+    , pc :: Word16 -- program counter
+    , i :: Word16 -- index register
     , stack :: [Word16] -- should this be in CHIP-8 RAM?
-    , delayTimer :: T.Timer
-    , soundTimer :: T.Timer
-    , vars :: UA.UArray Word Word8
+    , delayTimer :: Word8
+    , soundTimer :: Word8
+    , vars :: UA.UArray Word8 Word8 -- variable registers
     }
+
+displayWidth = 64
+displayHeight = 32
+
+newtype Instruction = Instruction { execute :: Machine -> Machine }
 
 makeMachine :: Machine
 makeMachine = Machine
     { ram = makeRam
     , screen = makeScreen
-    , pc = PC.makePc
+    , pc = 0x200
     , i = 0
     , stack = []
-    , delayTimer = T.set 0
-    , soundTimer = T.set 0
+    , delayTimer = 0
+    , soundTimer = 0
     , vars = makeVars
     }
 
@@ -33,7 +42,116 @@ makeRam :: UA.UArray Word16 Word8
 makeRam = UA.array (0, 4095) []
 
 makeScreen :: UA.UArray (Word8, Word8) Bool
-makeScreen = UA.array ((0, 0), (64, 32)) []
+makeScreen = UA.array ((0, 0), (displayWidth - 1, displayHeight - 1)) []
 
-makeVars :: UA.UArray Word Word8
+makeVars :: UA.UArray Word8 Word8
 makeVars = UA.array (0, 15) []
+
+load :: B.ByteString -> Machine -> Machine
+load bytes machine = machine { ram = newRam }
+    where
+      byteList = B.unpack bytes
+      assocs = zip [0x200..] byteList
+      newRam = ram machine // assocs
+
+cycle :: Machine -> Either String Machine
+cycle machine = do
+    let opcode = fetch machine
+    let newMachine = incrementPc machine
+    instruction <- decode opcode
+    pure $ execute instruction newMachine
+
+fetch :: Machine -> Word16
+fetch machine = xxyy byte1 byte2
+    where
+      mRam = ram machine
+      mPc = pc machine
+      byte1 = mRam ! mPc
+      byte2 = mRam ! (mPc + 1)
+
+incrementPc :: Machine -> Machine
+incrementPc machine = machine { pc = pc machine + 2 }
+
+decode :: Word16 -> Either String Instruction
+decode word = case word .&. 0xF000 of
+    0x0000 -> case word .&. 0xFFF of
+        0x0E0 -> Right $ Instruction clearScreen
+        _ -> Left $ "Unsupported machine language routine " ++ showHex word ""
+    0x1000 -> Right $ Instruction $ jump (iNNN word)
+    0x6000 -> Right $ Instruction $ setVar (iNii word) (iiNN word)
+    0x7000 -> Right $ Instruction $ addToVar (iNii word) (iiNN word)
+    0xA000 -> Right $ Instruction $ setI (iNNN word)
+    0xD000 -> Right $ Instruction $ draw (iNii word) (iiNi word) (iiiN word)
+    _ -> Left $ "Invalid opcode " ++ showHex word ""
+
+clearScreen :: Machine -> Machine
+clearScreen machine = machine { screen = makeScreen }
+
+jump :: Word16 -> Machine -> Machine
+jump newPc machine = machine { pc = newPc }
+
+setVar :: Word8 -> Word8 -> Machine -> Machine
+setVar x newVal machine = machine { vars = newVars }
+    where newVars = vars machine // [(x, newVal)]
+
+addToVar :: Word8 -> Word8 -> Machine -> Machine
+addToVar x addend machine = machine { vars = newVars }
+    where newVars = accum (+) (vars machine) [(x, addend)]
+
+setI :: Word16 -> Machine -> Machine
+setI newI machine = machine { i = newI }
+
+draw :: Word8 -> Word8 -> Word8 -> Machine -> Machine
+draw xVar yVar height machine = machine { screen = newScreen }
+    where
+      x = (vars machine ! xVar) `mod` displayWidth
+      y = (vars machine ! yVar) `mod` displayHeight
+      sprite = readSprite (ram machine) (i machine) height
+      newScreen = blit (screen machine) sprite x y
+
+readSprite 
+    :: UA.UArray Word16 Word8 
+    -> Word16 
+    -> Word8 
+    -> UA.UArray (Word8, Word8) Bool
+readSprite ram i height = bits
+    where
+      bytes16 = ixmap (0, fromIntegral (height - 1)) (+i) ram
+      bytes = ixmap (0, height - 1) fromIntegral bytes16
+        :: UA.UArray Word8 Word8
+      -- Each byte is an 8px-wide row, so our sprite should be 8*height
+      bitAssocs = [ ((x, i), b .&. (2^(7 - x)) /= 0)
+                    | (i, b) <- assocs bytes, x <- [0..7] ]
+      bits = array ((0, 0), (7, height - 1)) bitAssocs
+
+blit
+    :: UA.UArray (Word8, Word8) Bool
+    -> UA.UArray (Word8, Word8) Bool
+    -> Word8
+    -> Word8
+    -> UA.UArray (Word8, Word8) Bool
+blit screen sprite x y = accum 
+    (/=)
+    screen
+    [ ((ax + x, ay + y), s) | ((ax, ay), s) <- assocs sprite ]
+
+iNNN :: Word16 -> Word16
+iNNN = (0xFFF .&.)
+
+iNii :: Word16 -> Word8
+iNii x = fromIntegral $ shift (0xF00 .&. x) (-8)
+
+iiNN :: Word16 -> Word8
+iiNN = fromIntegral . (0xFF .&.)
+
+iiNi :: Word16 -> Word8
+iiNi x = fromIntegral $ shift (0xF0 .&. x) (-4)
+
+iiiN :: Word16 -> Word8
+iiiN = fromIntegral . (0xF .&.)
+
+xxyy :: Word8 -> Word8 -> Word16
+xxyy x y = shift x16 8 + y16
+    where
+      x16 = fromIntegral x :: Word16
+      y16 = fromIntegral y :: Word16
